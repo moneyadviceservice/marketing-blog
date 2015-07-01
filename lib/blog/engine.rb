edbe59fdbc94f70b7a5e5084b371a6a888efca27
David Kewal <david.kewal@gmail.com>
2015-07-01 11:36:12 +0100

    Refactor FactoryGirl path

--- lib/blog/engine.rb
+++ lib/blog/engine.rb
@@ -12,7 +12,7 @@ module Blog
 
     initializer :factories, after: 'factory_girl.set_factory_paths' do
       if defined?(FactoryGirl)
-        FactoryGirl.definition_file_paths << File.expand_path('../../../spec/factories', __FILE__)
+        FactoryGirl.definition_file_paths << root.join('spec', 'factories')
       end
     end
 
